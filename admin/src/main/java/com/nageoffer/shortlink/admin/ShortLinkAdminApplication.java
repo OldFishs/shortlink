0801c6179bc234e9fb61f96c6e40c5e8d44ed464
OldFishs <166841641+OldFishs@users.noreply.github.com>
2025-02-14 02:20:55 +0800

    feature:开发短链接后管系统相关功能

--- admin/src/main/java/com/nageoffer/shortlink/admin/ShortLinkAdminApplication.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/ShortLinkAdminApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
-@MapperScan("com.nageoffer.shortlink.admin.dao.mapper")
+@MapperScan ("com.nageoffer.shortlink.admin.dao.mapper")
 public class ShortLinkAdminApplication {
 
     public static void main(String[] args) {

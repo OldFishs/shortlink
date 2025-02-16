bee7c4630990c9ae1baab4e0bc2c539707658e6a
OldFishs <166841641+OldFishs@users.noreply.github.com>
2025-02-17 00:57:01 +0800

    feature:开发短链接后管系统用户退出登录功能

--- admin/src/main/java/com/nageoffer/shortlink/admin/common/enums/UserErrorCodeEnum.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/common/enums/UserErrorCodeEnum.java
@@ -10,7 +10,11 @@ public enum UserErrorCodeEnum implements IErrorCode {
 
     USER_EXIST("B00202","用户记录已存在"),
 
-    USER__SAVE_EXIST("B00203","用户记录新增失败");
+    USER_SAVE_EXIST("B00203","用户记录新增失败"),
+
+    USER_HAS_LOGIN("B20004","用户已登录"),
+
+    USER_NOEXIST_NOLOGIN("B20005","用户Token不存在或用户未登录");
 
     private final String code;
 
--- admin/src/main/java/com/nageoffer/shortlink/admin/service/UserService.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/service/UserService.java
@@ -66,4 +66,12 @@ public interface UserService extends IService<UserDO> {
      * @return 用户是否登录标识
      **/
     Boolean checkLogin(String username,String token);
+
+    /**
+     * 用户退出登录
+     *
+     * @param username 用户名
+     * @param token 用户登录token
+     **/
+    void logout(String username, String token);
 }

feb35f1d3f0ad9652dd2c3d51af1e3379baaa404
OldFishs <166841641+OldFishs@users.noreply.github.com>
2025-02-18 01:29:28 +0800

    feature:开发短链接后管分组排序字段

--- admin/src/main/java/com/nageoffer/shortlink/admin/service/impl/GroupServiceImpl.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/service/impl/GroupServiceImpl.java
@@ -25,6 +25,7 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper,GroupDO> implement
         } while (!hasGid(gid));
         GroupDO groupDO = GroupDO.builder()
                 .gid(gid)
+                .sortOrder(0)
                 .name(groupName)
                 .build();
         baseMapper.insert(groupDO);
